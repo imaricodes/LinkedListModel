e2ff6a01511907ca1a69cde8ffbb5fa6f0092a16
imari <imaricodes@gmail.com>
2016-10-12 15:12:59 -0700

    first commit

---
+++ LinkedListModel/LinkedLIstNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListModel
+{
+    //This is a node class. We create this so we can instantiate node objects
+    public class LinkedListNode<T>
+    {
+        //This is a constructor that takes a value of <some> type as a parameter   
+        public LinkedListNode (T value)
+        {
+            Value = value;
+        }
+
+        //this is a Property of the LinkedListNode object. Allows access to the value field of node
+        public T Value { get; set; }
+
+        //this is a Next property (or attribute) of the node object. Here is where we store the location of the next linked node (or 'null' if none).
+        public LinkedListNode<T> Next { get; set; }
+    }
+}
