7ff16c172e2fd5ec93d948d6e8a4245b1699c40c
imari <imaricodes@gmail.com>
2016-10-14 11:45:11 -0700

    add linked list class

---
+++ LinkedListModel/LinkedList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListModel
+{
+    public class LinkedList<T> : ICollection<T>
+    {
+        //properties
+
+        public LinkedListNode<T> Head { get; private set; }
+
+        public LinkedListNode<T> Tail { get; private set; }
+
+    }
+}
